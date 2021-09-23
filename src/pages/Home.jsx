057c87fea267833978400a23073bd356f8ce2f8d
Rohit Singh <rhtsing@gmail.com>
2021-09-23 18:19:01 +0530

    Chart Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import DataChart from "../components/DataChart";
 import InfoSection from "../components/InfoSection";
-import "./styleFolder/general.css"
+import "./styleFolder/general.css";
+
+import { userData } from "../dummyData";
 
 const Home = () => {
   return (
     <div className="home">
       <InfoSection />
+      <DataChart title="User Analytics" data={userData} dataKey="Active User" grid  />
     </div>
   )
 }
