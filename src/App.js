a76b55ffc799c4c2838257a4d2a39e40eeafe38e
Rohit Singh <rhtsing@gmail.com>
2021-09-27 21:22:27 +0530

    userTable

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import './App.css';
 import Topbar from './components/Topbar.jsx';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import UserTable from './pages/UserTable';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <div className="others">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/users">
+              <UserTable />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/pages/UserTable.jsx
@@ -0,0 +1,75 @@
+import { DataGrid } from "@material-ui/data-grid";
+import { DeleteOutline } from "@material-ui/icons";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { userRows } from "../dummyData";
+import "./styleFolder/user.css";
+
+
+const UserTable = () => {
+  const [ data, setData ] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 100 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 300,
+      renderCell: (params) => {
+        return (
+          <div className="userListUser">
+            <img className="userListImg" src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 170,
+    },
+    {
+      field: "transaction",
+      headerName: "Transaction Volume",
+      width: 200,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="userListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div style={{ height: '100%', width: '100%' }}>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={8}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  )
+}
+
+export default UserTable
